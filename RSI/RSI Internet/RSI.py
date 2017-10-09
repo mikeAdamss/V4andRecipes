48ace4f52dccf21c31364b78cec4f2e978c3c414
MikeData <Mikelovesbooks@gmail.com>
2017-10-09 15:42:22 +0100

    Finalised RSI transformations

--- RSI/RSI Internet/RSI.py
+++ RSI/RSI Internet/RSI.py
@@ -3,4 +3,9 @@
 from WDAtoV4 import makeV4
 import sys
 
-makeV4(sys.argv[1], geo="K03000001", presentation="labelsToCodes")
+df = makeV4(sys.argv[1], geo="K03000001", presentation="labelsToCodes", dropDims=[2,4,6], asDataFrame=True)
+
+headers = [x.replace('RSI Series type_codelist', 'Series type_codelist').replace('RSI Series type','Series type') for x in df.columns.values]
+df.columns = headers
+
+df.to_csv('Output_V4-WDADataset_Internet.csv', index=False)
--- RSI/RSI Main/RSI.py
+++ RSI/RSI Main/RSI.py
@@ -3,4 +3,4 @@
 from WDAtoV4 import makeV4
 import sys
 
-makeV4(sys.argv[1], geo="K03000001", presentation="labelsToCodes")
+makeV4(sys.argv[1], geo="K03000001", presentation="labelsToCodes", dropDims=[6])
